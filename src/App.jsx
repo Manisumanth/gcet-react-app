75c6507e58afc12405ea47f45649e7b8082394f3
Manisumanth <manisumanthk@gmail.com>
2025-06-05 14:12:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@ import { useState, createContext } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import Product from "./components/Product";
-import Product from "./components/Product";
-import Cart from "./components/Cart";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Logout from "./components/Logout";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+
 
 export const AppContext = createContext();
 
@@ -23,11 +16,6 @@ function App() {
         <Header /> 
         <main>
           <Routes>
-            <Route path="/" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/register" element={<Register />} />
           </Routes>
         </main>
 
